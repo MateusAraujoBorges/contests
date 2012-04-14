2f02814fbfc4d22ec3a93e5b22ed3652dc64406e
Mateus Borges <mateus.borges@sodet.biz>
2012-04-14 14:39:27 -0300

    solution for first problem of GCJ 2012

---
+++ codejam/2012/TONGUE.py
@@ -0,0 +1,16 @@
+#!/ust/bin/python
+import sys,string
+
+input = open(sys.argv[1])
+nentries = int(input.readline());
+keyfrom = ' acbedgfihkjmlonqpsrutwvyxz'
+keyto = ' yehosvcdxiulgkbzrntjwfpamq'
+
+table = string.maketrans(keyfrom,keyto)
+
+i = 1
+for line in input:
+    print "Case #" + str(i) + ": " + string.translate(line,table),
+    i = i + 1
+
+
